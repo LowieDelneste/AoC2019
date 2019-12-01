9295c3e8890549f75d138b201a4eeebdb40cc643
LowieDelneste <lowiedelneste@gmail.com>
2019-12-01 12:12:19 +0100

    Solution day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt").Select(x => double.Parse(x));
+
+            // Part 1
+            var sumFuelRequirements = input.Sum(x => CalculateFuel(x));
+            Console.WriteLine($"Fuel requirements: {sumFuelRequirements}");
+
+            // Part 2
+            var sumFuelRequirementsWithMass = input.Sum(x => CalculateFuel(x, 0));
+            Console.WriteLine($"Fuel requirements with mass: {sumFuelRequirementsWithMass}");
+        }
+
+        private static double CalculateFuel(double mass) => Math.Floor(mass / 3) - 2;
+
+        private static double CalculateFuel(double mass, double totalFuel)
+        {
+            var currentMass = CalculateFuel(mass);
+            if(currentMass <= 0)
+            {
+                return totalFuel;
+            }
+            return CalculateFuel(currentMass, totalFuel + currentMass) ;
+        }
+    }
+}
